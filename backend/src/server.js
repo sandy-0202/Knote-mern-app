3cd1460effbbac75b0d42c71aeaef110b6780239
sandy-0202 <sandy2122004@gmail.com>
2025-12-26 12:56:52 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path"
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB  } from "./config/db.js";
@@ -12,19 +13,29 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001
-
+const __dirname =path.resolve()
 
 
 //middleware
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-    })
-);
+if(process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+}
+
 app.use(express.json());
 
 app.use("/api/notes",rateLimiter,notesRoutes);
 
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"../frontend/dist","index.html"));
+})
+}
 
 // connect to DB before starting server
 connectDB().then(() => {app.listen(PORT,() => {
@@ -32,7 +43,7 @@ connectDB().then(() => {app.listen(PORT,() => {
     });
 });
 
-    
+
 
 
 
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+
+//made the url to be dynamic for deployement
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
 const api = axios.create({
-    baseURL: "http://localhost:5001/api"
+    baseURL: BASE_URL,
 })
 
 export default api;
